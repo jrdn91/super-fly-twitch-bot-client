fe069f469857e215665bd09061a7116ecbcb7fec
Jordan Riser <riser.jordan@gmail.com>
2015-08-18 11:13:01 -0400

    setting bootstrap theme with bootswatch addon

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-bootswatch': {
+      'theme': 'paper',
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
