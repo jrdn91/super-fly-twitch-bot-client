e7c8bf58ed028c996ff40ea1551fb0b4fef172be
Jordan Riser <riser.jordan@gmail.com>
2015-08-18 11:14:00 -0400

    added index template and controller with start and stop bot actions

---
+++ app/routes/index.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    startBot: function(){
+      console.log('starting bot');
+      Ember.$.ajax({
+        method: 'post',
+        dataType: 'json',
+        url: 'http://localhost:3000/api/v1/bot',
+        success: function(data) {
+          console.log(data);
+          // this.notify.success('Bot Started');
+        },
+        error: function(err) {
+          console.log(err);
+        }
+      });
+    },
+    stopBot: function(){
+      console.log('stoping bot');
+      Ember.$.ajax({
+        method: 'delete',
+        dataType: 'json',
+        url: 'http://localhost:3000/api/v1/bot',
+        success: function(data) {
+          console.log(data);
+          // this.notify.success('Bot Stopped');
+        },
+        error: function(err) {
+          console.log(err);
+        }
+      });
+    }
+  }
+});
