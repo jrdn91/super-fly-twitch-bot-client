5d8417bbd0d29a408b6ce71bf3fa063e2f1ed4bd
Jordan Riser <riser.jordan@gmail.com>
2015-09-01 07:29:54 -0400

    moved actions into controller so property binding works

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,56 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	isConnected: false,
+  actions: {
+    startBot: function(){
+      var self = this;
+      console.log('starting bot');
+      Ember.$.ajax({
+        method: 'post',
+        dataType: 'json',
+        url: 'http://localhost:3000/api/v1/bot',
+        success: function(data) {
+          console.log(data);
+          self.toggleProperty('isConnected');
+          self.get('notify').success('Bot started');
+        },
+        error: function(err) {
+          console.log(err);
+          self.get('notify').error('There was a problem');
+        }
+      });
+    },
+    stopBot: function(){
+      var self = this;
+      console.log('stoping bot');
+      Ember.$.ajax({
+        method: 'delete',
+        dataType: 'json',
+        url: 'http://localhost:3000/api/v1/bot',
+        success: function(data) {
+          console.log(data);
+          self.toggleProperty('isConnected');
+          self.get('notify').success('Bot stopped');
+        },
+        error: function(err) {
+          console.log(err);
+          self.get('notify').error('There was a problem');
+        }
+      });
+    },
+    showModal: function(name, model) {
+      this.render(name, {
+        into: 'application',
+        outlet: 'modal',
+        model: model
+      });
+    },
+    removeModal: function() {
+      this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
+  }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,53 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    startBot: function(){
-      var self = this;
-      console.log('starting bot');
-      Ember.$.ajax({
-        method: 'post',
-        dataType: 'json',
-        url: 'http://localhost:3000/api/v1/bot',
-        success: function(data) {
-          console.log(data);
-          self.get('notify').success('Bot started');
-        },
-        error: function(err) {
-          console.log(err);
-          self.get('notify').error('There was a problem');
-        }
-      });
-    },
-    stopBot: function(){
-      var self = this;
-      console.log('stoping bot');
-      Ember.$.ajax({
-        method: 'delete',
-        dataType: 'json',
-        url: 'http://localhost:3000/api/v1/bot',
-        success: function(data) {
-          console.log(data);
-          self.get('notify').success('Bot stopped');
-        },
-        error: function(err) {
-          console.log(err);
-          self.get('notify').error('There was a problem');
-        }
-      });
-    },
-    showModal: function(name, model) {
-      this.render(name, {
-        into: 'application',
-        outlet: 'modal',
-        model: model
-      });
-    },
-    removeModal: function() {
-      this.disconnectOutlet({
-        outlet: 'modal',
-        parentView: 'application'
-      });
-    }
-  }
+  
 });
